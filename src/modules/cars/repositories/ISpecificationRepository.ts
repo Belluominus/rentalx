5b9d46d854d5932e90ca4011024a1c804025c8d3
Belluominus <jpscbelluominus@gmail.com>
2022-02-23 17:51:48 -0300

    implementacao user

--- src/modules/cars/repositories/ISpecificationRepository.ts
+++ src/modules/cars/repositories/ISpecificationRepository.ts
@@ -1,12 +1,9 @@
+import { ICreateSpecificationDTO } from "../dtos/ICreateSpecificationDTO";
 import { Specification } from "../entities/Specification";
 
-interface ICreateSpecificationDTO {
-  name: string;
-  description: string;
-}
 interface ISpecificationRepository {
   findByname(name: string): Promise<Specification>;
   create({ description, name }: ICreateSpecificationDTO): Promise<void>;
 }
 
-export { ISpecificationRepository, ICreateSpecificationDTO };
+export { ISpecificationRepository };
